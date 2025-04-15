b1c59a322aaca43d9210ef612b6a86ef266f5735
ankitaasatkar <ankitaasatkar12@gmail.com>
2025-04-15 11:05:06 +0530

    Add files via upload

---
+++ SQL/MySql (15-04-25).sql
@@ -0,0 +1,57 @@
+select * from student;
+select * from courses;
+select * from employees;
+select * from student_data;
+
+## CREATE UNIQUE INDEX
+create unique index salary on employees(emp_id);
+create unique index student_name on student_data(id);
+create unique index new_name on employees(new_name);
+
+## CREATE INDEX
+create  index age on employees(age);
+create index address on student_data(address);
+create index age on student_data(age);
+create index Credits on courses(Credits);
+
+## DROP INDEX
+alter table student_data drop index address;
+alter table courses drop index Credits; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+create index stud on (salary);
+
+create unique index salary_1 on sales_data(id);
+
+alter table sales_data drop index sal;
+
+select * from sales_data;
+
+select * from sales_data where salary is not null;
+
+select * from sales_data where salary is null;
+
+
+
+
+
+
+
+
+
